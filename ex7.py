7f7abd86abc05f5749d80576791c01d23611ab71
elefnt <elefnt.ti@gmail.com>
2016-12-29 20:39:24 +0800

    More Printing

---
+++ ex7.py
@@ -0,0 +1,39 @@
+# print string
+print "Mary had a little lamb."
+# print string ,insert strings with format character
+print "Its fleece was white as %s."%'snow'
+# print sting
+print "And everywhere that Mary went."
+#print dots for ten times
+print "." * 10 # what'd that do?
+
+# name variable
+end1 = "C"
+# name variable
+end2 = "h"
+# name variable
+end3 = "e"
+# name variable
+end4 = "e"
+# name variable
+end5 = "s"
+# name variable
+end6 = "e"
+# name variable
+end7 = "B"
+# name variable
+end8 = "u"
+# name variable
+end9 = "r"
+# name variable
+end10 = "g"
+# name variable
+end11 = "e"
+# name variable
+end12 = "r"
+
+# print variables and conjuctions twn lines into one
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+# print variables
+print end7 + end8 + end9 + end10 + end11 + end12
