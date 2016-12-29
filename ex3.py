9949a70286ffef8950cde3717440768e856c930c
elefnt <elefnt.ti@gmail.com>
2016-12-29 17:11:35 +0800

    Numbers And Math

---
+++ ex3.py
@@ -0,0 +1,41 @@
+# output strings
+print "I will now count my chickens:"
+#blank line
+
+# output strings and number
+print "Hens", 25.0+30.0/6.0
+# output strings and its number
+print "Roosters",100.0-25.0*3.0%4.0
+
+# output strings
+print "Now I will count the eggs:"
+
+# output calculated results
+print 3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0
+
+# output strings
+print "Is it true that 3+2<5-7?"
+
+# output calculated results
+print 3.0+2.0<5.0-7.0
+
+# output calculated results
+print "What is 3+2?",3.0+2.0
+
+# output calculated results
+print 3.0+2.0<5.0-7.0
+
+# output strings and its calculated results
+print "What is 3+2?",3.0+2.0
+print "What is 5-7?",5.0-7.0
+
+# output strings
+print "Oh,that's why it's False."
+
+# output strings
+print "How about some more."
+
+# output strings and its calculated results
+print "Is it greater?",5.0>-2.0
+print "Is it greater or equal?",5.0>=-2.0
+print "Is it less or equal",5.0<=-2.0
