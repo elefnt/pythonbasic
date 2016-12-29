7faf45089afdb31cc7c34b36da57d0ba7fab5225
elefnt <elefnt.ti@gmail.com>
2016-12-29 17:07:39 +0800

    A Good First Program

---
+++ ex1.py
@@ -0,0 +1,12 @@
+# my first code
+print "Hello World!"
+print "Hello Again"
+
+# blank line
+print
+# here Again
+print "I like typing this."
+print "This is fun."
+print "Yay!Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
